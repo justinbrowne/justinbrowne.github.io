c21d5883c5626a82fed81459d32e5c43bac02893
Justin Browne <justinbrowne@live.com>
2017-02-04 12:50:32 -0800

    Add files via upload

--- cardform.js
+++ cardform.js
@@ -1,5 +1,6 @@
 var cardForm = document.getElementById("cardform");
-
+var errorMessageName = cardForm.firstChild.nextSibling.firstChild.nextSibling;
+var errorMessageCC = cardForm.firstChild.nextSibling.firstChild.nextSibling.nextSibling.nextSibling;
 
 cardForm.addEventListener("submit", function (event) {
 	event.preventDefault();
@@ -9,12 +10,16 @@ cardForm.addEventListener("submit", function (event) {
 	
 	if(isEmpty(cardName.value)) {
 		alert("Enter the name on your card.");
+		errorMessageName.classList.add("oops");
+	} else {
+		
 	}
 	
 	if(isCreditCard(cardNumber.value)) {
 	}
 	else {
 	alert("Enter a valid Credit Card number.");
+	errorMessageCC.classList.add("oops");
 	}
 });
 
